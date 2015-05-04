3bd44a4493676e603e24408e1a97268f9a9b672d
Nitish Gupta <rishoo@rishoo.(none)>
2015-05-04 16:54:57 -0400

    test code cleanup

--- src/test.h
+++ src/test.h
@@ -3,18 +3,22 @@
 
 #include <iostream>
 
-void test_hash();
-void check_insert_at();
+void test_hash_fwd(std::unordered_map<std::string, std::vector<long>>,std::unordered_map<std::string, std::vector<long>>,skip_list);
+void test_hash_rev(std::unordered_map<std::string, std::vector<long>> ,std::unordered_map<std::string, std::vector<long>> ,skip_list);
+void test_hash(std::unordered_map<std::string, std::vector<long>>,std::unordered_map<std::string, std::vector<long>>,skip_list);
 
-void check_search();
-void test_search_static_reference();
-void test_search_dynamic_reference();
-void test_search();
-
-void test_snp_at();
+//void check_insert_at(genome, std::string, long, std::string&);
 void test_insert_at();
 
-void check_delete_at();
+void check_search();
+void test_search();
+
+void test_search_static_reference();
+void test_search_dynamic_reference();
+
+void test_snp_at();
+
+//void check_delete_at(genome&, long, long, std::string&);
 void test_delete_at();
 
 void test();

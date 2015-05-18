8f39e591e7987ff70b5ad28cf3ed21683bb3f2eb
komal-s <komalsnehal1991@gmail.com>
2015-05-18 05:26:40 -0400

    added benchmarking code for extracting random substrings

--- dyn_sa/src/test.cpp
+++ dyn_sa/src/test.cpp
@@ -7,6 +7,7 @@
 #include "utils.h"
 #include <string.h>
 #include "types.h"
+#include <sstream>
 using namespace std;
 using namespace dynsa;
 
@@ -24,7 +25,7 @@ extern "C" {
 #include <sys/resource.h>
 #include <sys/time.h>
 
-#define QF 302348
+#define QF 4641
 #define QC 5000
 #define N 10
 
@@ -319,9 +320,9 @@ void benchmark_search(int argc, char *argv[]){
 	auto it = edits.begin();
 	auto q = queries.begin();
 
-	int queryfreq = QF;//argv[4];
-	int querycount = QC;//argv[5];
-	int iterations = N;//number_of_edits(argv[4]/argv[5]);
+	int queryfreq = QF;
+	int querycount = QC;
+	int iterations = N;
 
 	for(int i=0; i<iterations; i++){
 		
@@ -341,7 +342,6 @@ void benchmark_search(int argc, char *argv[]){
 	    			total_length_del += (get<2>(*it)-get<1>(*it));
 	    			it++;
 			}
-			//std::cerr << i*queryfreq+j << endl;	
 		} 
 		
 		gettimeofday(&start, &tzp);
@@ -394,11 +394,117 @@ void benchmark_search(int argc, char *argv[]){
 
 }
 
+void benchmark_substr(int argc, char *argv[]){
+
+	size_t n;
+	uchar *text;
+  	DynSA *wt;
+  	float *f;
+	size_t total_length_ins = 0, total_length_del = 0;
+    	struct timeval start, end;
+    	struct timezone tzp;
+
+  	if (argc < 3) {
+    		usage(argv[0]);
+  	}
+
+  	text = get_reference_sequence(argv[1],n,1);
+
+	// Creates the structure for dynamic suffix array
+	wt = new DynSA(false);
+	f = DynRankS::createCharDistribution(text, (n > 10000000) ? n+1 : 10000000, 1);
+	wt->initEmptyDynSA(f);
+	gettimeofday(&start, &tzp);
+	wt->addText(text,n);
+	gettimeofday(&end, &tzp);
+	print_time_elapsed("DynSA: Building Index: ", &start, &end);
+
+	int test_count=0;
+
+	//Read data
+  	vector<tuple<char, long, string>> input_data;
+  	get_test_input(argv[2],input_data);
+
+	gettimeofday(&start, &tzp);
+
+	for(auto it: input_data){
+
+		if(test_count>=stoi(argv[3],nullptr,10))
+			break;
+
+  		if(get<0>(it)=='I') {
+			//cout<<"Inserting "<<get<2>(it)<<" at "<<get<1>(it) << endl;
+			uchar *ins = new uchar[ (get<2>(it)).length()+1 ];
+			strcpy( (char*)ins, (get<2>(it)).c_str() );
+			wt->addChars(ins, get<2>(it).length(), get<1>(it)+2); //wt->addChars(patterns[i], length_patterns[i], ins_indexes[i]+1);
+   			total_length_ins += get<2>(it).length();   	
+   			test_count++;
+		}
+		if(get<0>(it)=='D') {
+			//cout<<"Deleting from "<< get<1>(it)+1 <<" to "<< stoi(get<2>(it))+1 << endl;
+			wt->deleteChars(stoi(get<2>(it),nullptr,10)-get<1>(it)+1, get<1>(it)+1); //wt->deleteChars(length_del[i], del_indexes[i]+1);
+    		total_length_del += (stoi(get<2>(it),nullptr,10)-get<1>(it));
+    		test_count++;
+		}
+		if(get<0>(it)=='S') {
+			//cout<<"SNP "<<get<2>(it)<<" at "<<get<1>(it) << endl;
+			uchar *ins = new uchar[ (get<2>(it)).length()+1 ];
+			strcpy( (char*)ins, (get<2>(it)).c_str() );
+			
+			//cout<<"Deleting from "<< get<1>(it) <<" to "<< (get<2>(it)).length()+get<1>(it)-1 << endl;
+			wt->deleteChars((get<2>(it)).length(), get<1>(it)+1);
+			//cout<<"Inserting "<<get<2>(it)<<" at "<<get<1>(it) << endl;
+			wt->addChars(ins, get<2>(it).length(), get<1>(it)+1);
+			test_count++;
+		}
+  	}
+	gettimeofday(&end, &tzp);
+	std::string message = std::string("DynSA: Updates: ");
+	print_time_elapsed(message, &start, &end);
+
+	//Now extract substrings
+    std::vector<std::pair<long,long>> substrings;
+    std::ifstream inputfile (argv[4]);
+    std::string line, pos, len;
+    if (inputfile.is_open())
+    {
+        while(getline(inputfile, line))
+        {
+            std::stringstream linestream(line);
+            getline(linestream, pos, ',');
+            getline(linestream, len, ',');
+            substrings.push_back(std::make_pair(stol(pos,nullptr,10),stol(len,nullptr,10)));
+            line.clear(); pos.clear(); len.clear();
+        }
+    }
+    else
+    {
+        std::cerr << "Houston, we have a problem opening the substring file" << std::endl;
+        exit(-1);
+    }
+
+    gettimeofday(&start, &tzp);
+    for(auto substr: substrings){
+        //std::cout << substr.first << " " << substr.second << std::endl;
+	wt->retrieveTextFactor(substr.first, substr.second);
+    }
+    gettimeofday(&end, &tzp);
+    message.clear();
+    message = "Extracted " + std::to_string(substrings.size()) + " substrings: ";
+    print_time_elapsed(message, &start, &end);
+
+
+	delete [] text;
+  	delete [] f;
+  	cout << "Done..." << endl;
+
+}
 
 //Edited for benchmarking with DynHash
 int main(int argc, char *argv[]) {
 
-	benchmark_edits(argc, argv);
+	//benchmark_edits(argc, argv);
 	//benchmark_search(argc, argv);
-
+	benchmark_substr(argc,argv);
 }
+

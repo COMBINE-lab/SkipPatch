595579e02e0b143d5a18f16b1d4a8d69a52b2720
komal-s <komalsnehal1991@gmail.com>
2015-09-19 05:24:04 -0400

    added command line arguments for query frequency, query count for search benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,24 @@
 using namespace ez;
 using namespace std;
 
-void wabi_example(){
+void wabi_example() {
 
 	genome example;
 	example.set_reference("AGCTTTTCATTCTGA");
 	example.construct_hash();
 	example.display_hash();
 
-	example.snp_at(4,"G");
+	example.snp_at(4, "G");
 	example.display_hash();
 
-	example.insert_at("TGAA",7);
+	example.insert_at("TGAA", 7);
 	example.display_hash();
 	example.get_skip_list().print_list();
 	example.get_skip_list().print_base_level();
 	example.display_updated_genome();
 
-	example.insert_at("AT",3);
-	example.insert_at("TAC",16);
+	example.insert_at("AT", 3);
+	example.insert_at("TAC", 16);
 	example.display_hash();
 	example.get_skip_list().print_list();
 	example.get_skip_list().print_base_level();
@@ -35,84 +35,95 @@ void wabi_example(){
 
 }
 
-int main(int argc, const char* argv[]){
+int main(int argc, const char* argv[]) {
 
 	ezOptionParser opt;
 
-    namespace spd = spdlog;
+	namespace spd = spdlog;
 
-    auto stderrLogger = spd::stdout_logger_mt("console");
-    //auto fileSink = std::make_shared<spd::sinks::simple_file_sink_mt>(logFileName, true);
-    //auto consoleSink = std::make_shared<spd::sinks::stderr_sink_mt>();
-    //auto consoleLog = spdlog::create("consoleLog", {consoleSink});
-    //auto fileLog = spdlog::create("fileLog", {fileSink});
-    //auto jointLog = spdlog::create("jointLog", {fileSink, consoleSink});
+	auto stderrLogger = spd::stdout_logger_mt("console");
+	//auto fileSink = std::make_shared<spd::sinks::simple_file_sink_mt>(logFileName, true);
+	//auto consoleSink = std::make_shared<spd::sinks::stderr_sink_mt>();
+	//auto consoleLog = spdlog::create("consoleLog", {consoleSink});
+	//auto fileLog = spdlog::create("fileLog", {fileSink});
+	//auto jointLog = spdlog::create("jointLog", {fileSink, consoleSink});
 
 	opt.overview = "Test dynamically-updateable index.";
 	opt.syntax = "main [OPTIONS]";
-
 	//opt.footer = "v0.1.4 Copyright (C) 2011 Remik Ziemlinski\nThis program is free and without warranty.\n";
 
-	opt.add( "", 0, 0, 0,
-             "Help!",
-             "-h", "-help", "--help", "--usage");
-	opt.add( "", 0, 0, 0,
-             "Build the hash.",
-             "-b", "--build");
-	opt.add( "", 0, 1, 0,
-             "The genome file.",
-             "-g", "--genome");
-	opt.add( "", 0, 1, 0,
-             "The edit file.",
-             "-e", "--edits");
-	opt.add( "", 0, 1, 0,
-             "The number of edits to perform.",
-             "-n", "--numEdits");
-	opt.add( "", 0, 1, 0,
-             "The substrings file.",
-             "-s", "--substr");
+	opt.add("", 0, 0, 0, "Help!", "-h", "-help", "--help", "--usage");
 
-    opt.parse(argc, argv);
+	//TODO: Do we need the build option? Don't we *have* to build the hash?
+	opt.add("", 0, 0, 0, "Build the hash.", "-b", "--build");
+	opt.add("", 0, 1, 0, "The genome file.", "-g", "--genome");
 
-    if (opt.isSet("-h") ) {
-        std::string usage;
-        opt.getUsage(usage,80,ezOptionParser::ALIGN);
-        std::cerr << usage;
-        return 1;
-    }
+	opt.add("", 0, 1, 0, "The edit file.", "-e", "--editsFile");
+	opt.add("", 0, 1, 0, "The number of edits to perform.", "-n", "--numEdits");
 
-    genome g;
+	opt.add("", 0, 1, 0, "The edit file which contains both edits and queries (for benchmarking search).", "-q", "--editsQueriesFile");
+	opt.add("", 0, 1, 0, "The query frequency (to be used only when benchmarking search).", "-qf", "--queryFrequency");
+	opt.add("", 0, 1, 0, "The query count (to be used only when benchmarking search).", "-qc", "--queryCount");
+	opt.add("", 0, 1, 0, "The number of iterations of edits and searches to perform (to be used only when benchmarking search).", "-it", "--iterations");
 
-    std::string genomeFile;
+	opt.add("", 0, 1, 0, "The substrings file.", "-s", "--substrFile");
 
-    if (opt.isSet("--genome")) {
-        opt.get("--genome")->getString(genomeFile);
-    } else {
-        stderrLogger->warn() << "You can't build an index without a genome!";
-        return 1;
-    }
+	opt.parse(argc, argv);
 
-    std::string editFile;
-    if (opt.isSet("--edits")) {
-        opt.get("--edits")->getString(editFile);
-    }
+	if (opt.isSet("-h")) {
+		std::string usage;
+		opt.getUsage(usage, 80, ezOptionParser::ALIGN);
+		std::cerr << usage;
+		return 1;
+	}
 
-    long numEdits;
-    if (opt.isSet("--numEdits")) {
-        opt.get("--numEdits")->getLong(numEdits);
-    }
-	
+	genome g;
+
+	std::string genomeFile;
+
+	if (opt.isSet("--genome")) {
+		opt.get("--genome")->getString(genomeFile);
+	} else {
+		stderrLogger->warn() << "You can't build an index without a genome!";
+		return 1;
+	}
+
+	std::string editsFile;
+	if (opt.isSet("--editsFile")) {
+		opt.get("--editsFile")->getString(editsFile);
+	}
+
+	long numEdits;
+	if (opt.isSet("--numEdits")) {
+		opt.get("--numEdits")->getLong(numEdits);
+	}
+
 	std::string substrFile;
-    if (opt.isSet("--substr")) {
-        opt.get("--substr")->getString(substrFile);
-    }
+	if (opt.isSet("--substrFile")) {
+		opt.get("--substrFile")->getString(substrFile);
+	}
 
-    g.get_input(genomeFile);
-	
-    test();
+	long queryFrequency;
+	if (opt.isSet("--queryFrequency")) {
+		opt.get("--queryFrequency")->getLong(queryFrequency);
+	}
+
+	long queryCount;
+	if (opt.isSet("--queryCount")) {
+		opt.get("--queryCount")->getLong(queryCount);
+	}
+
+	long iterations;
+	if (opt.isSet("--iterations")) {
+		opt.get("--iterations")->getLong(iterations);
+	}
+
+	g.get_input(genomeFile);
+
+	//test();
 	//benchmark_edits(g,editFile,numEdits);
 	//benchmark_substring(g,substrFile);
-	//benchmark_search(g, editFile);
+	//benchmark_search(g, editsFile, queryFrequency, queryCount, iterations);
 
 	return 0;
 }
